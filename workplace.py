b04a076d14f277c1bc3e438b62bac23f3e3b173d
EVO <evo@evos-mac-pro.local>
2022-07-20 11:10:19 +0800

    Update workplace.py - Add year statement

--- workplace.py
+++ workplace.py
@@ -2,3 +2,9 @@ def investment_solver(year, init_money, interest_rate, investment_return):
     remain_money = investment_return
     year_temp = 0
     invest_temp = investment_return
+    if year == "X":
+        while remain_money - init_money > 0:
+            remain_money = (invest_temp / (1 + interest_rate))
+            year_temp += 1
+            invest_temp = remain_money
+        return year_temp
