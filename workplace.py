791085285a77364abf27db1d3511e376c3411f23
EVO <evo@evos-mac-pro.local>
2022-07-20 10:57:28 +0800

    Update workplace.py - Add def CF_Solver

--- workplace.py
+++ workplace.py
@@ -6,3 +6,15 @@ def C_to_F(C):
 def F_to_C(F):
     C = (F - 32) * (5 / 9)
     return C
+
+
+def CF_Solver(C, F):
+    if C == "X":
+        C_Result = F_to_C(F)
+        print(C_Result)
+    if F == "X":
+        F_Result = C_to_F(C)
+        print(F_Result)
+
+
+CF_Solver("X", 73)
