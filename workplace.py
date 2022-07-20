0b85e446dbfa46443673b107b4bd240df8e91fe8
EVO <evo@evos-mac-pro.local>
2022-07-20 11:08:31 +0800

    Update workplace.py - Add def investment_solver

--- workplace.py
+++ workplace.py
@@ -1,20 +1,4 @@
-def C_to_F(C):
-    F = (C * (9 / 5)) + 32
-    return F
-
-
-def F_to_C(F):
-    C = (F - 32) * (5 / 9)
-    return C
-
-
-def CF_Solver(C, F):
-    if C == "X":
-        C_Result = F_to_C(F)
-        print(C_Result)
-    if F == "X":
-        F_Result = C_to_F(C)
-        print(F_Result)
-
-
-CF_Solver("X", 73)
+def investment_solver(year, init_money, interest_rate, investment_return):
+    remain_money = investment_return
+    year_temp = 0
+    invest_temp = investment_return
